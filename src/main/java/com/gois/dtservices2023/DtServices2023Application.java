85c7c4e760af54301b7d5ee59e97971438463207
Gustavo Meneses Gois <gustavo.gois@gmail.com>
2023-01-11 11:56:38 -0300

    Initial commit

---
+++ src/main/java/com/gois/dtservices2023/DtServices2023Application.java
@@ -0,0 +1,13 @@
+package com.gois.dtservices2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtServices2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtServices2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gois/dtservices2023/DtServices2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gois.dtservices2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtServices2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
